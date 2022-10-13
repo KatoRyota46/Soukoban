f2c5fa997e20ed2b853459a1f21dac03213a9cae
KatoRyota46 <106728621+KatoRyota46@users.noreply.github.com>
2022-10-13 10:17:26 +0900

    Add files via upload

---
+++ PlayerMove.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    /*プレイヤー移動の基本操作
+     * 別スクリプトの"PlayerMoveInputSystem"からステータスを得る。
+     */
+}
